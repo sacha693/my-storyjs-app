e80226f295ab3d282f72a3033619272f01e95c4e
sacha693 <sachalai@gmail.com>
2026-05-10 21:58:30 +0800

    Remove index and itinerary cache references

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,42 @@
-const CACHE_NAME='kansai-trip-pwa-v3';
+const CACHE_NAME='kansai-trip-pwa-v12';
 const urlsToCache=[
 './',
-'./index.html',
-'./itinerary.html',
+'./main.html',
 './guide.html',
 './usj.html',
 './quick.html',
 './today.html',
 './reminders.html',
+'./expense.html',
 './manifest.json'
 ];
 
 self.addEventListener('install',event=>{
-event.waitUntil(
- caches.open(CACHE_NAME)
- .then(cache=>cache.addAll(urlsToCache))
- .then(()=>self.skipWaiting())
-);
+ event.waitUntil(
+  caches.open(CACHE_NAME)
+   .then(cache=>cache.addAll(urlsToCache))
+   .then(()=>self.skipWaiting())
+ );
 });
 
 self.addEventListener('activate',event=>{
-event.waitUntil(
- caches.keys().then(keys=>Promise.all(
-  keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
- ))
-);
-self.clients.claim();
+ event.waitUntil(
+  caches.keys().then(keys=>Promise.all(
+   keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
+  ))
+ );
+ self.clients.claim();
 });
 
 self.addEventListener('fetch',event=>{
  event.respondWith(
   caches.match(event.request)
-  .then(response=>response||fetch(event.request)
-   .then(networkResponse=>{
-    const responseClone=networkResponse.clone();
-    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,responseClone));
-    return networkResponse;
-   })
-  ).catch(()=>caches.match('./itinerary.html'))
+   .then(response=>response||fetch(event.request)
+    .then(networkResponse=>{
+      const responseClone=networkResponse.clone();
+      caches.open(CACHE_NAME).then(cache=>cache.put(event.request,responseClone));
+      return networkResponse;
+    })
+   ).catch(()=>caches.match('./main.html'))
  );
 });
