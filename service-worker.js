067f5dd724c60ff1e8908225acd6c6c3c0cdcf2b
sacha693 <sachalai@gmail.com>
2026-05-10 21:19:40 +0800

    Upgrade service worker for offline PWA

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,42 @@
-const CACHE_NAME = 'kansai-trip-v1';
-const urlsToCache = ['/', '/index.html'];
+const CACHE_NAME='kansai-trip-pwa-v3';
+const urlsToCache=[
+'./',
+'./index.html',
+'./itinerary.html',
+'./guide.html',
+'./usj.html',
+'./quick.html',
+'./today.html',
+'./reminders.html',
+'./manifest.json'
+];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install',event=>{
+event.waitUntil(
+ caches.open(CACHE_NAME)
+ .then(cache=>cache.addAll(urlsToCache))
+ .then(()=>self.skipWaiting())
+);
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('activate',event=>{
+event.waitUntil(
+ caches.keys().then(keys=>Promise.all(
+  keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
+ ))
+);
+self.clients.claim();
+});
+
+self.addEventListener('fetch',event=>{
+ event.respondWith(
+  caches.match(event.request)
+  .then(response=>response||fetch(event.request)
+   .then(networkResponse=>{
+    const responseClone=networkResponse.clone();
+    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,responseClone));
+    return networkResponse;
+   })
+  ).catch(()=>caches.match('./itinerary.html'))
+ );
 });
