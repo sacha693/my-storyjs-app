1c9001dcb7a4534bd84f84a294e0a556f0fa2294
sacha693 <sachalai@gmail.com>
2026-05-21 21:34:10 +0800

    Connect itinerary component system to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Hero } from '../components/Hero'
 import { NavGrid } from '../components/NavGrid'
 import { ExpenseSummary } from '../components/ExpenseSummary'
+import { DaySection } from '../components/DaySection'
 import { navigationItems } from '../data/navigation'
 
 export function HomePage() {
@@ -12,6 +13,8 @@ export function HomePage() {
       />
 
       <NavGrid items={navigationItems} />
+
+      <DaySection />
 
       <ExpenseSummary />
     </main>
