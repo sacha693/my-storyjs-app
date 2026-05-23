242b1f53dc600fc03e6d88e939580221cca6fe17
sacha693 <sachalai@gmail.com>
2026-05-23 15:15:31 +0800

    Switch to modular CSS entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles.css'
+import './styles/index.css'
 import { AppRouter } from './router'
 
 const redirectPath = sessionStorage.getItem('spa-redirect-path')
